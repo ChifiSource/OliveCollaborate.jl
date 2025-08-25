fc3754ebd4879d5bdca93ee196f8c6c1b86948b9
emmy <52672675+emmaccode@users.noreply.github.com>
2025-08-25 14:03:05 -0600

    bind/focus update!
    There are a few last things i want to do, and several style adjustments and general adjustments to make, but my lordt this is getting close!!

---
+++ src/RPCProjects.jl
@@ -0,0 +1,245 @@
+function get_collaborator_data(c::Connection, proj::Project{:rpc})
+    projs = c[:OliveCore].users[proj[:host]].environment.projects
+    pf = findfirst(p -> typeof(p) == Project{:collab}, projs)
+    rpcinfo_proj = projs[pf]
+    allinfo = rpcinfo_proj[:cells][1].outputs
+    splitinfo = split(allinfo, ";")
+    just_me = findfirst(s -> contains(split(s, "|")[1], getname(c)), splitinfo)
+    split(splitinfo[just_me], "|")::Vector{SubString{String}}
+end
+
+function get_collaborator_data(c::Connection, name::String, proj::Project{:rpc})
+    projs = c[:OliveCore].users[proj[:host]].environment.projects
+    pf = findfirst(p -> typeof(p) == Project{:collab}, projs)
+    rpcinfo_proj = projs[pf]
+    allinfo = rpcinfo_proj[:cells][1].outputs
+    splitinfo = split(allinfo, ";")
+    just_me = findfirst(s -> contains(split(s, "|")[1], name), splitinfo)
+    split(splitinfo[just_me], "|")::Vector{SubString{String}}
+end
+
+function set_rpc_cellfocus!(c::AbstractConnection, proj::Project{<:Any}, cell::Cell{<:Any}, comp::Component{<:Any})
+    cellid = cell.id
+    childs = comp[:children]
+    if "cellinterior$cellid" in childs
+       interior = comp[:children, "cellinterior$cellid"]
+       intchildren = interior[:children]
+       if "cellinput$cellid" in intchildren
+            comp = interior[:children, "cellinput$cellid"][:children, "cell$cellid"]
+       end
+    elseif "cell$cellid" in childs
+        comp = comp[:children, "cell$cellid"]
+    end
+    on(c, comp, "focus") do cm::ComponentModifier
+        color = get_collaborator_data(c, cell.outputs, proj)[4]
+        style!(cm, "cellcontainer$cellid", "border" => "2px solid $color", 
+            "border-radius" => 3px)
+        cm["cell$cellid"] = "contenteditable" => "false"
+        call!(c, cm)
+    end
+    on(c, comp, "focusout") do cm::ComponentModifier
+        style!(cm, "cellcontainer$cellid", "border" => "0px")
+        cm["cell$cellid"] = "contenteditable" => "true"
+        call!(c, cm)
+    end
+    nothing::Nothing
+end
+
+function build(c::AbstractConnection, cm::ComponentModifier, p::Project{:rpc})
+    frstcells::Vector{Cell} = p[:cells]
+    retvs = Vector{Servable}([begin
+       built = CORE.olmod.build(c, cm, cell, p)
+       set_rpc_cellfocus!(c, p, cell, built)
+       built::Component{<:Any}
+    end for cell in frstcells])
+    div(p.id, children = retvs, class = "projectwindow")::Component{:div}
+end
+
+function cell_bind!(c::Connection, cell::Cell{<:Any}, proj::Project{:rpc})
+    keybindings = c[:OliveCore].users[Olive.getname(c)].data["keybindings"]
+    km = ToolipsSession.KeyMap()
+    cells = proj[:cells]
+    ToolipsSession.bind(km, keybindings["save"], prevent_default = true) do cm::ComponentModifier
+        Olive.save_project(c, cm, proj)
+        rpc!(c, cm)
+    end
+    ToolipsSession.bind(km, keybindings["up"]) do cm2::ComponentModifier
+        Olive.cell_up!(c, cm2, cell, proj)
+        rpc!(c, cm2)
+    end
+    ToolipsSession.bind(km, keybindings["down"]) do cm2::ComponentModifier
+        Olive.cell_down!(c, cm2, cell, proj)
+        rpc!(c, cm2)
+    end
+    ToolipsSession.bind(km, keybindings["delete"]) do cm2::ComponentModifier
+        Olive.cell_delete!(c, cm2, cell, cells)
+        rpc!(c, cm2)
+    end
+    ToolipsSession.bind(km, keybindings["new"]) do cm2::ComponentModifier
+        pos = findfirst(lcell -> lcell.id == cell.id, cells)
+        creator_cell = Cell{:creator}()
+        if pos == length(cells)
+            push!(cells, creator_cell)
+        else
+            insert!(cells, pos + 1, creator_cell)
+        end
+        callcell = Cell{:callcreator}(Olive.getname(c), id = creator_cell.id)
+        insert!(cm2, proj.id, pos + 1, build(c, cm2, callcell, proj))
+        call!(c, cm2)
+        insert!(cm2, proj.id, pos + 1, build(c, cm2, creator_cell, proj))
+        focus!(cm2, "cell$(creator_cell.id)")
+    end
+    ToolipsSession.bind(km, keybindings["evaluate"]) do cm2::ComponentModifier
+        cellid::String = cell.id
+        if "load$(cellid)" in cm2
+            return
+        end
+        icon = Olive.olive_loadicon()
+        icon.name = "load$cellid"
+        icon["width"] = "16"
+        proj.data[:mod].WD = CORE.users[getname(c)].environment.pwd
+        append!(cm2, "cellside$(cellid)", icon)
+        on(c, cm2, 100) do cm2::ComponentModifier
+            Olive.evaluate(c, cm2, cell, proj)
+            remove!(cm2, "load$cellid")
+            rpc!(c, cm2)
+        end
+        rpc!(c, cm2)
+    end
+
+    ToolipsSession.bind(km, keybindings["focusup"]) do cm::ComponentModifier
+        Olive.focus_up!(c, cm, cell, proj)
+    end
+    ToolipsSession.bind(km, keybindings["focusdown"]) do cm::ComponentModifier
+        Olive.focus_down!(c, cm, cell, proj)
+    end
+    km::ToolipsSession.KeyMap
+end
+
+function build(c::Connection, cm::ComponentModifier, cell::Cell{:creator},
+    proj::Project{:rpc})
+    cells = proj[:cells]
+    windowname::String = proj.id
+    creatorkeys = CORE.users[getname(c)]["creatorkeys"]
+    cbox = Components.textdiv("cell$(cell.id)", text = "")
+    style!(cbox, "outline" => "transparent", "color" => "white")
+    key = ToolipsSession.get_session_key(c)
+    on(c, cbox, "input") do cm2::ComponentModifier
+        txt = cm2[cbox]["text"]
+        if txt in keys(creatorkeys)
+            pos = findfirst(lcell -> lcell.id == cell.id, cells)
+            cellt = creatorkeys[txt]
+            new_cell = Cell(string(cellt), "")
+            session = Olive.SES
+            mock_ses = ToolipsSession.MockSession(session)
+            host_id, host_event = ToolipsSession.find_host(c, true)
+            for client in (host_id, host_event.clients ...)
+                if client == key
+                    continue
+                end
+                tempdata = Dict{Symbol, Any}(:Session => session, :OliveCore => c[:OliveCore], :SESSIONKEY => client)
+                newcon = Connection(c.stream, tempdata, Vector{Toolips.Route}(), get_ip(c))
+                client_cell = build(newcon, cm2, new_cell, proj)
+                set_rpc_cellfocus!(newcon, proj, new_cell, client_cell)
+                insert!(cm2, windowname, pos, client_cell)
+                remove!(cm2, buttonbox)
+                call!(c, cm2, client)
+            end
+            remove!(cm2, buttonbox)
+            deleteat!(cells, pos)
+            insert!(cells, pos, new_cell)
+            client_cell = build(c, cm2, new_cell, proj)
+            set_rpc_cellfocus!(c, proj, new_cell, client_cell)
+            insert!(cm2, windowname, pos, client_cell)
+            focus!(cm2, "cell$(new_cell.id)")
+            # finished func!
+         elseif txt != ""
+             Olive.olive_notify!(cm2, "$txt is not a recognized cell hotkey", color = "red")
+             set_text!(cm2, cbox, "")
+        end
+    end
+    km = cell_bind!(c, cell, proj)
+    ToolipsSession.bind(c, cm, cbox, km)
+    olmod = CORE.olmod
+     buttonbox = div("cellcontainer$(cell.id)")
+     push!(buttonbox, cbox)
+     push!(buttonbox, h3("spawn$(cell.id)", text = "new cell"))
+     group_excluded_sigs = Olive.get_group(c).cells
+     for m in methods(Olive.build, [Toolips.AbstractConnection, Toolips.Modifier, Olive.IPyCells.AbstractCell, Project{<:Any}])
+        sig = m.sig.parameters[4]
+         if sig == Cell{<:Any}
+             continue
+         end
+         if ~(is_jlcell(sig))
+            continue
+         end
+         signature::Symbol = sig.parameters[1]
+         if sig in group_excluded_sigs
+            continue
+         end
+         b = button("$(sig)butt", text = string(signature))
+         on(c, b, "click") do cm2::ComponentModifier
+             pos = findfirst(lcell -> lcell.id == cell.id, cells)
+             remove!(cm2, buttonbox)
+             new_cell = Cell(string(signature), "")
+             deleteat!(cells, pos)
+             insert!(cells, pos, new_cell)
+             insert!(cm2, windowname, pos, build(c, cm2, new_cell,
+              proj))
+         end
+         push!(buttonbox, b)
+     end
+     buttonbox
+end
+
+function build(c::Connection, cm::ComponentModifier, cell::Cell{:callcreator},
+    proj::Project{:rpc})
+    label = h3(text = cell.source * " is creating a cell")
+    collabdata = get_collaborator_data(c, cell.outputs, proj)
+    style!(label, "color" => collabdata[4])
+    bod = div("cellcontainer$(cell.id)", children = [label])
+    style!(bod, "border" => "3px solid $(collabdata[4])")
+    bod::Component{:div}
+end
+
+function cell_bind!(c::Connection, cell::Cell{:getstarted}, proj::Project{:rpc})
+    keybindings = c[:OliveCore].users[Olive.getname(c)].data["keybindings"]
+    km = ToolipsSession.KeyMap()
+    cells = proj[:cells]
+    ToolipsSession.bind(km, keybindings["evaluate"]) do cm2::ComponentModifier
+        Olive.evaluate(c, cm2, cell, proj)
+        rpc!(c, cm2)
+    end
+    km::ToolipsSession.KeyMap
+end
+
+function cell_highlight!(c::Connection, cm::ComponentModifier, cell::Cell{:code}, proj::Project{:rpc})
+    windowname::String = proj.id
+    curr = cm["cell$(cell.id)"]["text"]
+    cursorpos = parse(Int64, cm["cell$(cell.id)"]["caret"])
+    cell.source = curr
+    if length(cell.source) == 0
+        return
+    end
+    tm = c[:OliveCore].users[getname(c)].data["highlighters"]["julia"]
+    set_text!(tm, curr)
+    OliveHighlighters.mark_julia!(tm)
+    set_text!(cm, "cellhighlight$(cell.id)", string(tm))
+    if cursorpos == 0
+        curspos = 1
+    end
+    n = length(curr)
+    set_text!(tm, cell.source[1:cursorpos])
+    OliveHighlighters.mark_julia!(tm)
+    first_half = string(tm)
+    set_text!(tm, cell.source[cursorpos + 1:end])
+    OliveHighlighters.mark_julia!(tm)
+    second_half = string(tm)
+    OliveHighlighters.clear!(tm)
+    collabdata = get_collaborator_data(c, proj)
+    ToolipsSession.call!(c, cm) do cm2::ComponentModifier
+        hltxt = first_half * "<a style='color:$(collabdata[4]);'>▆</a>" * second_half
+        set_text!(cm2, "cellhighlight$(cell.id)", hltxt)
+        set_text!(cm2, "cell$(cell.id)", curr)
+    end
+end
